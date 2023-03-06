38b829804976ad9d78953bd8457b9e4993d1e2cf
Mikkel Bistrup Andersen <mbia@itu.dk>
2023-03-06 10:47:35 +0100

    Solved exercises 5.1-5.4

---
+++ Exercises.fsx
@@ -0,0 +1,32 @@
+//Exercise 5.1
+let sum m n = 
+    let rec aux acc n = 
+        match n with 
+        | 0 -> acc + m 
+        | _ -> aux (acc + (m + n)) (n-1) 
+    aux 0 n
+
+//Exercise 5.2
+let length lst =
+    let rec aux lst acc =
+        match lst with
+        | [] -> acc
+        | x::xs -> aux xs (acc+1)
+    aux lst 0
+
+
+//Exercise 5.3
+let foldBack f lst acc =
+    let rec aux lst c =
+        match lst with
+        | [] -> c acc
+        | x::xs -> aux xs (fun r -> c (f x r))
+    aux lst id
+
+//Exercise 5.4
+let factA x =
+    let rec aux acc =
+        function
+        | 0 -> acc
+        | x -> aux (x * acc) (x - 1)
+    aux 1 x
